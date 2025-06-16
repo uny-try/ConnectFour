83eee3a8c9d7322632423a7c6f94803f8a28968a
Tamura Yuji <uny.nippon@gmail.com>
2025-06-16 21:30:37 +0900

    Start project

---
+++ ConnectFour/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ConnectFour;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        const int newheight = 800;
+        const int newwidth = 600;
+
+        var wins = new Window(new AppShell());
+        wins.Height = wins.MinimumHeight = wins.MaximumHeight = newheight;
+        wins.Width = wins.MinimumWidth = wins.MaximumWidth = newwidth;
+        return wins;
+    }
+}
